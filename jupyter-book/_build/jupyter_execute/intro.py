59e2eb07fecd12ff106113c7714c3acbfc9f81af
Gyesik Lee <gslee@hknu.ac.kr>
2022-03-05 01:37:34 +0900

    ch01 being prepared

---
+++ jupyter-book/_build/jupyter_execute/intro.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 강의 소개
+
+# 머신러닝/딥러닝 기술이 획기적으로 발전하면서 데이터 분석 및 인공지능 관련 연구의 
+# 중요성이 사회, 경제, 산업의 거의 모든 분야에 지대한 영향을 미치고 있으며,
+# 앞으로 그런 경향이 더욱 강화될 것으로 기대된다.
+# 
+# 본 강의는 머신러닝/딥러닝 기본 아이디어와 다양한 활용법을 실전 예제와 함께 전달한다. 
+# 머신러닝/딥러닝 학습을 위해 아래 분야의 기초지식이 요구되지만 여기서는 필요한 최소의 내용만 다룬다.
+# 
+# - 파이썬 프로그래밍
+# - 미적분학, 선형대수, 확률과통계
+
+# **데이터 과학, 인공지능, 머신러닝, 딥러닝**
+
+# 아래 그림은 인공지능, 머신러닝, 딥러닝, 데이터 과학 네 분야의 관계를 설명한다.
+
+# <div align="center"><img src="https://github.com/codingalzi/handson-ml3/blob/master/jupyter-book/imgs/ai-ml-relation.png?raw=true" style="width:500px;"></div>
+# 
+# 그림 출처: [교보문고: 에이지 오브 머신러닝](http://www.kyobobook.co.kr/readIT/readITColumnView.laf?thmId=00198&sntnId=14142)
+
+# * 데이터 과학: 주어진 데이터로부터 수학과 통계 지식을 활용하여 필요한 정보를 추출하는 학문 분야
+# * 인공지능: 사고(thinking), 학습(learning) 등 인간의 지적능력을 컴퓨터를 통해 구현하는 
+#     기술 또는 해당 연구 분야
+# * 머신러닝: 컴퓨터가 데이터로부터 스스로 정보를 추출하는 기법 또는 해당 연구 분야.
+# * 딥러닝: 심층 신경망 이론을 기반으로 복잡한 비선형 문제를 해결하는 머신러닝 기법 
+#     또는 해당 연구 분야    
+
+# 역사적 관점에서 바라본 인공지능, 머신러닝, 딥러닝의 관계는 다음과 같다.
+# 
+# <div align="center"><img src="https://github.com/codingalzi/handson-ml3/blob/master/jupyter-book/imgs/ai-ml-relation2.png?raw=true" style="width:600px;"></div>
+# 
+# 
+# 그림 출처: [NVIDIA 블로그](https://blogs.nvidia.com/blog/2016/07/29/whats-difference-artificial-intelligence-machine-learning-deep-learning-ai/)
+
+# **주요 학습내용**
+
+# * 한눈에 보는 머신러닝
+# * 머신러닝 프로젝트 처음부터 끝까지
+# * 분류
+# * 모델 훈련
+# * 서포트 벡터 머신
+# * 결정 트리
+# * 앙상블 학습과 랜덤 포레스트
+# * 차원 축소
+# * 비지도 학습
